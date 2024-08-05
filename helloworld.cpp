205a70f9a9d503ac3dccad74eb857997995c0646
0xethjayadithya_g7/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-08-06 02:08:55 +0530

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+
+//why write iostream include header file library, as we can make use of input certain things, feeded to the program
+using namespace std;
+//std is like defining a scope
+int main(){
+    //main is a function which is returning a int here
+    cout<<"hello world!"; //print hello world
+    return 0; //means successful program into the OS without bug/error
+}
+
+//note: comments are ignored by compiler
+/*
+comments (multiline)
+works this way too 
+*/
