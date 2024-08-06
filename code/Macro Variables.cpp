424421e018e7d0e5df23b0ec059206703e05977b
0xethjayadithya_g7dev/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-08-06 11:00:08 +0530

    Create Macro Variables.cpp

---
+++ code/Macro Variables.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#define CHESS_WIDTH 10 //Defining Macro variables
+
+int main(){
+
+    cout<< CHESS_WIDTH <<endl; //preprocessor directory, it will update all the values during compile time
+    
+    return 0;
+}
