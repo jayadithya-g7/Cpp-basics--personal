59a3787168b383308bdffbe35ae3b31f84497da7
0xethjayadithya_g7dev/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-08-06 11:28:51 +0530

    Create logical operator.cpp

---
+++ logical operator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//arithmetic operators
+int main(){
+
+//logical operators
+int a =20;
+bool checkvalue = (a==17);
+cout<<checkvalue;
+/*
+so it checks if it nmatches, if it did, 
+then it prints 1 for true, 0 for false
+*/
+return 0;
+}
