8af984410eec6fe9b0f6261cae940db54bba6d73
0xethjayadithya_g7dev/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-08-09 11:37:25 +0530

    Create Loop basics.cpp

---
+++ code/Loop basics.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main(){
+    // int a,b=10; //how to take userinputs thru cin
+    // cout<<"enter your no.";
+    // cin >>a;
+    // int sum = a+b;
+    // cout<<"your sum is: "<<sum;
+    
+    
+   //
+    //step-1 : while loop
+    // int counter=1;
+    // int sum=0;
+    // cout<<"enter choice of n until which the loop should run:";
+    // int n;
+    // cin>>n;
+    // while(counter<=n){
+    //     sum+=counter;
+    //     counter+=1;
+    // }
+    // cout<<"sum[1,n] is: "<<sum<<endl; //hence we get sum of n natural no.s
+
+
+
+
+
+
+// let's check out do while loop, here the condition is executed at the last
+// do{
+// //write your logic
+// }while(condition);
+
+//so here atleast while executes one time, unless normal while loop
+
+
+
+
+
+
+
+
+
+
+
+
+//step-2
+//for loop
+//for( initialization ; condition ; after loop execution)
+
+// int sum=0;
+// int n;
+// cout<<"enter the value of n until which the loop should execute: ";
+// cin>>n;
+
+// for(int counter=1; counter<=n; counter++){
+//     sum+=counter;
+// }
+// cout<<"sum[1,n] = "<<sum<<endl;
+
+    return 0;
+}
